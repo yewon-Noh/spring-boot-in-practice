9d4fe3a373c8b134f8f0bd4c0d0d9a7890a31686
yewon-Noh <yewo2nn16@gmail.com>
2024-02-02 15:16:03 +0900

    ch03: 스프링부트 어플리케이션 데이터베이스 연동 설정(mysql, jpa)

---
+++ ch03/jpa-mysql/src/test/java/com/example/jpamysql/JpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
