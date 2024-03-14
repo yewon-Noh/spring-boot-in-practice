3335193fa907511f213fc49b812b6313b2849602
yewon-Noh <yewo2nn16@gmail.com>
2024-03-14 19:30:54 +0900

    ch05: 스프링 시큐리티 설정 커스터마이징

---
+++ ch05/spring-security-test/src/main/java/com/example/springsecuritytest/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.springsecuritytest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/index").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
